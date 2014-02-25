7be9e777b76ad9de308a484bc42d8a043826e7bd
littlepretty yan <littlepretty881203@gmail.com>
2014-02-25 18:07:43 +0800

    Env as ID Environment

--- includes/symbols.hpp
+++ includes/symbols.hpp
@@ -6,17 +6,23 @@
 typedef enum tag
 {
 	AND = 256, BASIC, BREAK, DO, ELSE, EQ, FALSE, GE, IN, IF,
+	BIT_AND, BIT_OR, NOT, ASSIGN, GT, LT, 
 	INDEX, LE, MINUS, NE, NUM, OR, REAL, TEMP, TRUE, WHILE
+
 } Tag;
 
 class Token
 {
 public:
-	Token(Tag t):tag(t) {} ;
+	Token(Tag t):t_tag(t) {} ;
 	virtual ~Token();
-	virtual std::string toString() { return "" + (char)tag; }
+	virtual std::string toString() { 
+		std::stringstream result;
+		result<<t_tag;
+		return result.str(); 
+	}
 private:
-	const Tag tag;
+	const Tag t_tag;
 };
 
 class Num : public Token
@@ -26,52 +32,92 @@ public:
 	~Num();
 	virtual std::string toString() { 
 		std::stringstream result;
-		result<<Token::toString<<" value = "<<value;
+		result<<Token::toString()<<" value = "<<value;
 		return result.str();
 	}
 private:
 	const int value;
 };
 
-class Word
+class Word : public Token
 {
 public:
 	Word(std::string str, Tag t):Token(t), lexme(str) {};
 	virtual ~Word();
 	virtual std::string toString(){
-		return Token::toString + lexme;
+		return Token::toString() + lexme;
+	}
+
+	bool operator==(const Word& rhs) {
+		return this->lexme.compare(rhs.lexme) == 0;
 	}
 
 private:
 	std::string lexme;
+
+	const static Word WordAND;
+	const static Word WordOR;
+	const static Word WordEQ;
+	const static Word WordNE;
+	const static Word WordLE;
+	const static Word WordGE;
+	const static Word WordMINUS;
+	const static Word WordTRUE;
+	const static Word WordFALSE;
+	const static Word WordTEMP;
 	
 };
 
-static Word WordAND("&&", AND);
-static Word WordOR("||", OR);
-static Word WordEQ("==", EQ);
-static Word WordNE("!=", NE);
-static Word WordLE("<=", LE);
-static Word WordGE(">=", GE);
-static Word WordMINUS("minus", MINUS);
-static Word WordTRUE("true", TRUE);
-static Word WordFALSE("false", FALSE);
-static Word WordTEMP("temp", TEMP);
+Word Word::WordAND("&&", AND);
+Word Word::WordOR("||", OR);
+Word Word::WordEQ("==", EQ);
+Word Word::WordNE("!=", NE);
+Word Word::WordLE("<=", LE);
+Word Word::WordGE(">=", GE);
+Word Word::WordMINUS("minus", MINUS);
+Word Word::WordTRUE("true", TRUE);
+Word Word::WordFALSE("false", FALSE);
+Word Word::WordTEMP("temp", TEMP);
 
-class Real
+class Real : public Token
 {
 public:
 	Real(float val):Token(REAL), value(val) {};
 	~Real();
 	virtual std::string toString(){
 		std::stringstream result;
-		result<<Token::toString<<" value = "<<value;
+		result<<Token::toString()<<" value = "<<value;
 		return result.str();
 	}
 private:
 	float value;
 };
 
+class Env
+{
+public:
+	Env(Env* n):prev(n);
+	~Env();
+	void put(Token w, Id, i) {
+		table[w] = i;
+	}
+	Id get(Token w) {
+		for (Env* e = this; e != NULL; e=e->prev)
+		{
+			std::map<Token, Id>::iterator found = e->table.find(w);
+			if (found != std::map<Token, Id>::end())
+			{
+				return found->second;
+			}
+		}
+		return NULL;
+	}
+protected:
+	Env* prev;
+private:
+	std::map<Token, Id> table;
+
+};
 
 
 class Type: public Word
@@ -80,7 +126,6 @@ public:
 	Type(std::string str, Tag t, int w):Word(str,t),width(w) {};
 	~Type();
 
-private:
 	int width;
 };
 
@@ -97,13 +142,16 @@ public:
 private:
 	int size;
 	Type of;
-	
+	static Type TypeINT;
+	static Type TypeFLOAT;
+	static Type TypeCHAR;
+	static Type TypeBOOL;
 };
 
-static Type TypeINT("int", BASIC, 4);
-static Type TypeFLOAT("float", BASIC, 4);
-static Type TypeCHAR("char", BASIC, 1);
-static Type TypeBOOL("bool", BASIC, 1);
+Type Type::TypeINT("int", BASIC, 4);
+Type Type::TypeFLOAT("float", BASIC, 4);
+Type Type::TypeCHAR("char", BASIC, 1);
+Type Type::TypeBOOL("bool", BASIC, 1);
 
 bool numeric(Type p) 
 {
